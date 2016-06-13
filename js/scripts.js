5e406345d18b0bb87ccd3753cff69dc6216e254f
Rick Thornbrugh and Rouz Majlessi <rick+rouz@>
2016-06-13 15:21:37 -0700

    added divison and multiplication funciton

--- js/scripts.js
+++ js/scripts.js
@@ -16,4 +16,21 @@ var add = function(number1, number2){
  var number2 = parseInt(prompt("Enter another number"));
 
  alert(subtract(number1, number2));
- 
+
+
+ var multiply = function(number1, number2){
+  	return number1 * number2
+  };
+
+ var number1 = parseInt(prompt("Enter a number:"));
+ var number2 = parseInt(prompt("Enter another number"));
+
+ alert(multiply(number1, number2));
+
+ var divide = function(numnber1, number2){
+	return number1 / number2;
+ };
+ var number1 = parseInt(prompt("Enter a number"));
+ var number2 = parseInt(prompt("Enter another number"));
+
+ alert(divide(number1, number2));
